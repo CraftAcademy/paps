ca65bd5c7f03900c2f1cc6d306ba1c08d6cc2bc4
Isabelle <isabellelidberg@live.se>
2018-03-15 10:34:16 +0100

    adds home controller adds feature file adds step definitions for signup

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'pg', '>= 0.18', '< 2.0'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'coveralls', require: false
 gem 'devise'
+gem 'turbolinks'
+
 group :development, :test do
   gem 'factory_bot_rails'
   gem 'pry-byebug'
---
+++ features/step_definitions/signup_steps.rb
@@ -0,0 +1,27 @@
+Given("I visit the site") do
+  visit root_path
+end
+
+Then("I click {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I fill {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should see message {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I click {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I fill {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
