a2401e4634d2e3eef33a0976eaeff59705c5b6f9
kalle <kmthorsnes@gmail.com>
2018-03-15 11:36:54 +0100

    add url to sign up

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -6,15 +6,14 @@ Then("I click {string}") do |string|
   click_button string
 end
 
-Then("I fill {string} with {string}") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("I fill {string} with {string}") do |input, value|
+  fill_in input, with: value
 end
 
 Then("I should see message {string}") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-
-When("I fill {string} with {string}") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("show me the page") do
+  save_and_open_page
 end
