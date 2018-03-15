c6ac4aa3d311f25b705f0ac1eaf7e29a81f1befc
Isabelle <isabellelidberg@live.se>
2018-03-15 17:20:43 +0100

    delete test for id in user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'db table' do
-    it {is_expected.to have_db_column :id}
     it {is_expected.to have_db_column :email}
     it {is_expected.to have_db_column :encrypted_password}
   end
