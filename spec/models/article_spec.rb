07181902dfc3cd6e0a6e7f1f052d7dd010d63391
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-03-15 16:19:42 +0100

    add article unit spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB columns' do
+    it { is_expected.to have_db_column :title }
+    it { is_expected.to have_db_column :content }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryBot.create(:article)).to be_valid
+    end
+  end
 end
