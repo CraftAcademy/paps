4242955f1c6119df8c4542944a552916d8ff6d31
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-03-18 18:32:25 +0100

    create author table add author ref to article fix author tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Article, type: :model do
 
   describe 'Factory' do
     it 'should have valid Factory' do
-      expect(create(:author)).to be_valid
+      expect(create(:article)).to be_valid
     end
   end
 
