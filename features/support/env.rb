e910c8b5671ddb4e130785e170c4dca2c39c49ed
kalle <kmthorsnes@gmail.com>
2018-03-18 11:07:45 +0100

    revert changes

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,23 +15,16 @@ Cucumber::Rails::Database.javascript_strategy = :truncation
 
 Chromedriver.set_version '2.36'
 
-# Capybara.register_driver :selenium do |app|
-#   options = Selenium::WebDriver::Chrome::Options.new(
-#       implicit_wait: 60,
-#       args: %w( disable-popup-blocking disable-infobars)
-#   )
-#
-#   Capybara::Selenium::Driver.new(
-#       app,
-#       browser: :chrome,
-#       options: options
-#   )
-# end
-
 Capybara.register_driver :selenium do |app|
-  profile = Selenium::WebDriver::Chrome::Profile.new
-  client = Selenium::WebDriver::Remote::Http::Default.new
-  client.timeout = 90 # instead of the default 60
-  Capybara::Selenium::Driver.new(app, browser: :chrome, profile: profile, http_client: client)
+  options = Selenium::WebDriver::Chrome::Options.new(
+      implicit_wait: 60,
+      args: %w( disable-popup-blocking disable-infobars)
+  )
+
+  Capybara::Selenium::Driver.new(
+      app,
+      browser: :chrome,
+      options: options
+  )
 end
 Capybara.javascript_driver = :selenium
