43c3821f25b40dedac57856e93a60aa44f97dffe
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-03-18 20:49:36 +0100

    edit article path in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     if @article.save
       flash[:success] = "Article was successfully created."
-      redirect_to @article
+      redirect_to article_path(@article)
     else
       flash[:error] = "Field cannot be blank"
       render 'new'
