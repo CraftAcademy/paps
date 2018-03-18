823c6d8f94ca29522668f467d2e26e6ecfe782be
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-03-18 16:27:13 +0100

    add author field to new article add author column to article db display author on article show page validate and sanitze author params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,6 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params[:article].permit(:title, :content)
+    params[:article].permit(:title, :content, :author)
   end
 end
