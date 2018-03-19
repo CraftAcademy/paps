6212abfd5c2e1df3f1069f2aba4b7944a3c28b73
Antonella Morittu <morittuantonella@gmail.com>
2018-03-19 11:12:12 +0100

    add rspec for comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Comment, type: :model do
     it { is_expected.to have_db_column :email }
     it { is_expected.to have_db_column :content }
   end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :content }
+  end
+
+  describe "Factory" do
+    it "should have a valid factory" do
+      article = FactoryBot.create(:article)
+      expect(FactoryBot.create(:comment, article: article)).to be_valid
+    end
+  end
 end
