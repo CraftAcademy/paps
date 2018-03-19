b4f7f638e2219cf82db1cd60cd60ecad54d02b9d
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-03-19 10:23:41 +0100

    rollback author db changes change unnecessary code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_18_173006) do
+ActiveRecord::Schema.define(version: 2018_03_18_150215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,12 +20,6 @@ ActiveRecord::Schema.define(version: 2018_03_18_173006) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "author"
-    t.bigint "author_id"
-    t.index ["author_id"], name: "index_articles_on_author_id"
-  end
-
-  create_table "authors", force: :cascade do |t|
     t.string "author"
   end
 
@@ -47,5 +41,4 @@ ActiveRecord::Schema.define(version: 2018_03_18_173006) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "articles", "authors"
 end
