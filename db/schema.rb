a2c4f27dcae3672c66f747d7d43f19925bef145e
Holger_Tidemand <tidemand.holger@gmail.com>
2018-03-20 14:09:07 +0100

    adds feture test for location based news adds geocoder gem adds migration for adding lat & lng to article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_18_155404) do
+ActiveRecord::Schema.define(version: 2018_03_20_130642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2018_03_18_155404) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "comments", force: :cascade do |t|
