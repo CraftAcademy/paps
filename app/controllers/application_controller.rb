f096505dedea007e236f2b2ef9619309098e2675
Antonella Morittu <morittuantonella@gmail.com>
2018-03-21 14:58:19 +0100

    update application controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
 
+  def user_not_authorized
+      redirect_to root_path, notice: 'You are not authorized to perform that action!'
+    end
+
+    def current_user
+      super || create_guest_user
+    end
+
+    def create_guest_user
+      token = SecureRandom.base64(15)
+      user = User.new({password: token, email: "#{token}@example.com"}.merge!(@coordinates))
+      user.save(validate: false)
+      user
+    end
 end
