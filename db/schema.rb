b3676dab1e387482a6b2d4324438e32660c16410
Antonella Morittu <morittuantonella@gmail.com>
2018-03-21 15:09:31 +0100

    add migration AddAddressToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_20_130642) do
+ActiveRecord::Schema.define(version: 2018_03_21_140839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,9 @@ ActiveRecord::Schema.define(version: 2018_03_20_130642) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "subscriber", default: false
+    t.float "latitude"
+    t.float "longitude"
+    t.text "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
