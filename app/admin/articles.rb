8e4369797e50c46dc71daa1fbb67cf2246ec016e
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-03-22 11:26:55 +0100

    permit article params for admin

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -1,8 +1,6 @@
 ActiveAdmin.register Article do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
+
+permit_params :title, :content, :user_id
 #
 # or
 #
