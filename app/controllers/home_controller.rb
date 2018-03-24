cc6c2f679e9fd14f581e2637a8216bf020f040a7
Holger_Tidemand <tidemand.holger@gmail.com>
2018-03-24 15:21:42 +0100

    refactored home controller for better readability

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,14 @@
 class HomeController < ApplicationController
   before_action :get_coordinates, only: [:index]
+  GOTHENBURG = [57.700501, 11.975463]
 
   def index
-     if !@coordinates.empty?
-       user = create_guest_user
-       location = (current_user ? current_user.address : user.address)
-       @local_articles = Article.near(location, 20)
-       @articles = Article.all
-     else
-       @articles = Article.all
-     end
-  end
-
-  def get_location
-    user = create_guest_user
-    current_user ? current_user.address : user.address
+   @local_articles = Article.near(current_user.address, 20) if !@coordinates.empty?
+   @articles = Article.all
   end
 
   def set_edition
-    if User.near([57.700501, 11.975463], 50).include? current_user
+    if User.near(GOTHENBURG, 50).include? current_user
       @edition = 'Gothenburg Edition'
     else
       @edition = 'Rest of Sweden Edition'
@@ -38,11 +28,8 @@ class HomeController < ApplicationController
   end
 
   def update_user_location
-    if current_user
-      current_user.latitude = @coordinates.values.first
-      current_user.longitude = @coordinates.values.second
-      current_user.save
-    end
+    current_user.latitude = @coordinates.values.first
+    current_user.longitude = @coordinates.values.second
+    current_user.save
   end
-
 end
