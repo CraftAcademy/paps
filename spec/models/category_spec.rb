39c6547027bfef72838b2c9af7d2f27d1a05043a
Holger_Tidemand <tidemand.holger@gmail.com>
2018-03-24 16:04:14 +0100

    cleaned up in unit test for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   describe "Factory" do
     it "should have a valid factory" do
-      article = create(:category)
       expect(create(:category)).to be_valid
     end
   end
