8a98b863567e8daf763054de5e5e3a43bde899b4
Holger_Tidemand <tidemand.holger@gmail.com>
2018-03-27 16:48:53 +0200

    error with cucumber

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,8 +12,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-  config.include FactoryBot::Syntax::Methods
   config.include JSONAPI::RSpec
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
