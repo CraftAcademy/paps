118b88024f51f48c90c654baac89703e6913d643
Holger_Tidemand <tidemand.holger@gmail.com>
2018-03-28 11:51:11 +0200

    refactored article serilizer as requested

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,9 +1,8 @@
 class ArticleSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
+  include   Rails.application.routes.url_helpers
   attributes :title, :link
 
   def link
-    Rails.application.routes.url_helpers.api_v1_article_path(object)
+  api_v1_article_path(object)
   end
-
 end
