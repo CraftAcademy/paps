11dfd19aed8e1b7461e25cf151e21b45c4440008
Antonella Morittu <morittuantonella@gmail.com>
2018-03-28 15:37:15 +0200

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_23_105116) do
+ActiveRecord::Schema.define(version: 2018_03_21_152001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,21 +54,6 @@ ActiveRecord::Schema.define(version: 2018_03_23_105116) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_articles_on_user_id"
-  end
-
-  create_table "articles_categories", id: false, force: :cascade do |t|
-    t.bigint "article_id", null: false
-    t.bigint "category_id", null: false
-    t.index ["article_id", "category_id"], name: "index_articles_categories_on_article_id_and_category_id"
-    t.index ["category_id", "article_id"], name: "index_articles_categories_on_category_id_and_article_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
@@ -97,11 +82,9 @@ ActiveRecord::Schema.define(version: 2018_03_23_105116) do
     t.float "latitude"
     t.float "longitude"
     t.text "address"
-    t.string "role", default: "Visitor"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
 end
