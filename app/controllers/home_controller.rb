5b69a80a06720a0e56fa7915aa28eb27f5bed643
Holger_Tidemand <tidemand.holger@gmail.com>
2018-03-28 11:08:28 +0200

    add back old code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,21 @@ class HomeController < ApplicationController
   before_action :get_coordinates, only: [:index]
 
   def index
-     @articles = Article.all
+     if !@coordinates.empty?
+      user = create_guest_user
+       location = (current_user ? current_user.address : user.address)
+       @local_articles = Article.near(location, 20)
+       @articles = Article.all
+     else
+       @articles = Article.all
+     end
   end
 
+  def get_location
+    user = create_guest_user
+    current_user ? current_user.address : user.address
+  end
+  
   def set_edition
     if User.near([57.700501, 11.975463], 50).include? current_user
       @edition = 'Gothenburg Edition'
