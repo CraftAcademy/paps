d518c350be15dfc8c79c7663a032e62cb06aa807
Holger_Tidemand <tidemand.holger@gmail.com>
2018-03-29 11:25:39 +0200

    adds sad path to test for if user types in wrong password/email

--- spec/requests/api/v1/user_auth_spec.rb
+++ spec/requests/api/v1/user_auth_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Api::V1::ArticlesController, type: :request do
     end
   end
 
-  describe 'user signs in with /api/v1/auth/sign_in' do
+  describe 'user signs in with successfully' do
     let(:document) { JSON.parse(response.body) }
     let(:user) { create(:user) }
     let(:credentials) { user.create_new_auth_token }
@@ -28,6 +28,31 @@ RSpec.describe Api::V1::ArticlesController, type: :request do
       expected_response = eval(file_fixture('user.txt').read)
 
       expect(document).to eq expected_response
+    end
+  end
+
+  describe 'users types in the wrong credentials' do
+    let(:document) { JSON.parse(response.body) }
+    let(:user) { create(:user) }
+    let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+
+
+    it 'should return error if wrong password' do
+
+      post '/api/v1/auth/sign_in', params: {
+        email: user.email, password: 'wrongPassword',
+      }, headers: headers
+
+      expect(document['errors'].first).to eq 'Invalid login credentials. Please try again.'
+    end
+
+    it 'should return error if wrong email' do
+
+      post '/api/v1/auth/sign_in', params: {
+        email: 'wrong@email.com', password: user.password,
+      }, headers: headers
+
+      expect(document['errors'].first).to eq 'Invalid login credentials. Please try again.'
     end
 
   end
